b66e5a9ae460ca97cc98f453810b30437edec240
ElenaVlasenko <radionovaelena124@gmail.com>
2024-12-08 16:08:21 +0300

    fix test data gens

--- src/test/test-data-generators.ts
+++ src/test/test-data-generators.ts
@@ -1,12 +1,13 @@
+import { CATEGORY, LEVEL, TYPE } from '../const';
 import { Camera, OrderParams, Review } from '../types';
-import { intBetween, makeCounter, makeList, makeUniqStringGenerator } from './utils';
+import { getRandomArrayElement, intBetween, makeCounter, makeList, makeUniqStringGenerator } from './utils';
 
 const generateUniqCameraName = makeUniqStringGenerator((n) => `camera-name-${n}`);
 const generateUniqVendorCode = makeUniqStringGenerator((n) => `vendor-code-${n}`);
-const generateUniqType = makeUniqStringGenerator((n) => `type-${n}`);
-const generateCategory = makeUniqStringGenerator((n) => `category-${n}`);
+const generateType = () => getRandomArrayElement(Object.values(TYPE));
+const generateCategory = () => getRandomArrayElement(Object.values(CATEGORY));
 const generateDescription = makeUniqStringGenerator((n) => `description-${n}`);
-const generateLevel = makeUniqStringGenerator((n) => `level-${n}`);
+const generateLevel = () => getRandomArrayElement(Object.values(LEVEL));
 const generatePreviewImg = makeUniqStringGenerator((n) => `camera-preview-imj-${n}.jpeg`);
 const generatePreviewImg2x = makeUniqStringGenerator((n) => `camera-preview-imj-2x-${n}.jpeg`);
 const generatePreviewImgWebp = makeUniqStringGenerator((n) => `camera-preview-imj-${n}.webp`);
@@ -20,10 +21,10 @@ export const generateCamera = (params: Partial<Camera> = {}): Camera => {
     id,
     name: generateUniqCameraName(id),
     vendorCode: generateUniqVendorCode(id),
-    type: generateUniqType(id),
-    category: generateCategory(id),
+    type: generateType(),
+    category: generateCategory(),
     description: generateDescription(id),
-    level: generateLevel(id),
+    level: generateLevel(),
     price: intBetween(1000, 200000),
     rating: intBetween(1, 5),
     reviewCount: intBetween(0, 10),
